0a82771afb232fabf38f2d2063dfd941291da970
HasanR <hrahimi@360insights.com>
2023-07-19 15:23:00 -0400

    initial app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/courses/CoursesPage';
+import Header from './components/common/Header';
+import PageNotFound from './components/PageNotFound';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Header />
+      <Routes>
+        <Route exact path='/'>
+          <Route index element={<HomePage />} />
+          <Route path='about' element={<AboutPage/>} />
+          <Route path='courses' element={<CoursesPage/>} />
+          <Route path='*' element={<PageNotFound/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
