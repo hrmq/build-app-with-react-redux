108c94ffe3a6cd9829ee07878feccbf2f39c1fae
HasanR <hrahimi@360insights.com>
2023-07-19 19:45:03 -0400

    dispatch - manual mapping

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -21,7 +21,7 @@ class CoursesPage extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-        this.props.dispatch(courseActions.createCourse(this.state.course))
+        this.props.createCourse(this.state.course)
     }
 
     render() {
@@ -55,4 +55,10 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(CoursesPage)
+function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: course => dispatch(courseActions.createCourse(course))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
