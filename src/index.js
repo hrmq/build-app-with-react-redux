4bcfc8aafd93a8af213fcf066ce81762148f18d5
HasanR <hrahimi@360insights.com>
2023-07-19 19:41:52 -0400

    initial redux store, reducer and action

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { Provider as ReduxProvider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
+const store = configureStore()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ReduxProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </ReduxProvider>
   </React.StrictMode>
 );
 
